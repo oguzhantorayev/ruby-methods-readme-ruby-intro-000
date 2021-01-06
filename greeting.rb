78398ddcea5f896794b0384654a9a96cbc247d02
OGUZHAN TORAYEV <torayev.oguzhan@gmail.com>
2021-01-06 00:01:06 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+def helloworld
+  phrase "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
